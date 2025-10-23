a098aa3a9250ff5e5d6bd81869ec1d74e879ac28
05c4r-1337 <oscar.carl.johansson@gmail.com>
2025-10-23 18:00:17 +0200

    added dbcontext, userentity and a migration

---
+++ TownSquare/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using TownSquare.Models;
+
+namespace TownSquare.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configure your entity models here
+    }
+}
